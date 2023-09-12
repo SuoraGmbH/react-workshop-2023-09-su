5f0d96abdd735035003c70cdd2fab77843bf7182
Hans-Christian Otto <c.otto@suora.com>
2023-09-12 16:34:00 +0200

    remove old code

--- src/components/GithubRepoStats.tsx
+++ src/components/GithubRepoStats.tsx
@@ -8,17 +8,10 @@ const GithubRepoStats: React.FunctionComponent<Props> = ({ repo }) => {
   const [stars, setStars] = useState(4711);
 
   useEffect(() => {
-    // fetch(`https://api.github.com/repos/${repo}`)
-    //   .then((response) => response.json())
-    //   .then((data) => data.stargazers_count)
-    //   .then(setStars);
-    // .then((stargazers_count) => setStars(stargazers_count));
-
-    fetch(`https://api.github.com/repos/${repo}`).then((response) => {
-      response.json().then((data) => {
-        setStars(data.stargazers_count);
-      });
-    });
+    fetch(`https://api.github.com/repos/${repo}`)
+      .then((response) => response.json())
+      .then((data) => data.stargazers_count)
+      .then(setStars);
   }, [repo]);
 
   console.log("GithubRepoStats rendert grade");
