83998f59a2b76c654509974cc94454a1a8e90151
Hans-Christian Otto <c.otto@suora.com>
2023-09-12 16:33:47 +0200

    alternative syntax

--- src/components/GithubRepoStats.tsx
+++ src/components/GithubRepoStats.tsx
@@ -8,6 +8,12 @@ const GithubRepoStats: React.FunctionComponent<Props> = ({ repo }) => {
   const [stars, setStars] = useState(4711);
 
   useEffect(() => {
+    // fetch(`https://api.github.com/repos/${repo}`)
+    //   .then((response) => response.json())
+    //   .then((data) => data.stargazers_count)
+    //   .then(setStars);
+    // .then((stargazers_count) => setStars(stargazers_count));
+
     fetch(`https://api.github.com/repos/${repo}`).then((response) => {
       response.json().then((data) => {
         setStars(data.stargazers_count);
